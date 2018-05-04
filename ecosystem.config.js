c5069a1b7b9f51d486887b53ec25676b5d5a9947
beeBurrito <brian.stahly@gmail.com>
2018-05-04 12:30:44 -0700

    Tweak to ecosystem config

--- ecosystem.config.js
+++ ecosystem.config.js
@@ -1,13 +1,13 @@
 module.exports = {
   apps: [{
     name: 'tutorial-2',
-    script: './index.js'
+    script: 'index.js'
   }],
   deploy: {
     production: {
       user: 'ubuntu',
       host: 'ec2-18-237-106-26.us-west-2.compute.amazonaws.com',
-      key: '~/.ssh/TestKeyPair.pem',
+      key: 'C:/Users/Brian/.ssh/TestKeyPair.pem',
       ref: 'origin/master',
       repo: 'https://github.com/bstahly/NodeTutorial.git',
       path: '/home/ubuntu/tutorial-2',
