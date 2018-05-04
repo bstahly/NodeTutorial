469673dda78ba5add1521dd9bddda872640b60ac
beeBurrito <brian.stahly@gmail.com>
2018-05-04 12:13:17 -0700

    Setting up auto-deploy with ps2

---
+++ ecosystem.config.js
@@ -0,0 +1,17 @@
+module.exports = {
+  apps: [{
+    name: 'tutorial-2',
+    script: './index.js'
+  }],
+  deploy: {
+    production: {
+      user: 'ubuntu',
+      host: 'ec2-18-237-106-26.us-west-2.compute.amazonaws.com',
+      key: '~/.ssh/TestKeyPair.pem',
+      ref: 'origin/master',
+      repo: 'https://github.com/bstahly/NodeTutorial.git',
+      path: '/home/ubuntu/tutorial-2',
+      'post-deploy': 'npm install && pm2 startOrRestart ecosystem.config.js'
+    }
+  }
+}
